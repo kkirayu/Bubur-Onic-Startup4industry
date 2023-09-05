a7a0864889ec0d26dcff6aad87013a39e0b42ee8
shaddamalbz <shaddam.albz@gmail.com>
2023-09-05 10:42:15 +0700

    alurkerja color

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,21 @@ export default {
         'main-blue': { alurkerja: '#0095E8' },
         'tifany-blue': { alurkerja: '#17BCB4' },
         green: { alurkerja: '#50CD89' },
+        'forst-white': { alurkerja: '#F3F6F9' },
         'light-blue': { alurkerja: '#E1F0FF' },
+        black: {
+          alurkerja: {
+            1: '#1E1E2D',
+            2: '#3F4254',
+          },
+        },
+        grey: {
+          alurkerja: {
+            1: '#7E8299',
+            2: '#B5B5C3',
+            3: '#E4E6EF',
+          },
+        },
       },
     },
   },
