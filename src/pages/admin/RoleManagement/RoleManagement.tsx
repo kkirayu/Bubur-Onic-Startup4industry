3aa28ebb92eb3b25b86bab90a81b322a0013f39f
shaddamalbz <shaddam.albz@gmail.com>
2023-09-07 15:50:19 +0700

    feat: view list role

--- src/pages/admin/RoleManagement/RoleManagement.tsx
+++ src/pages/admin/RoleManagement/RoleManagement.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { TableLowcode } from 'alurkerja-ui'
+import { useState } from 'react'
 
-const RoleManagement = () => {
-  return <div>RoleManagement</div>
+function RoleManagement() {
+  const [pageConfig, setPageConfig] = useState({ limit: 10, page: 0 })
+  const [renderState, setRenderState] = useState(0)
+  const [filterBy, setFilterBy] = useState<{ [x: string]: any }>()
+  const [search, setSearch] = useState<string>()
+
+  return (
+    <section className="bg-white">
+      <TableLowcode
+        baseUrl={import.meta.env.VITE_API_BASEURL}
+        tableName="role"
+        renderState={renderState}
+        setRenderState={setRenderState}
+        pageConfig={pageConfig}
+        setPageConfig={setPageConfig}
+        filterBy={filterBy}
+        setFilterBy={setFilterBy}
+        search={search}
+        setSearch={setSearch}
+      />
+    </section>
+  )
 }
 
 export default RoleManagement
