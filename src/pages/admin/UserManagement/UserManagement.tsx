a7b1ce2f23ce6620149ea296a3b8e9ebab5d7d85
shaddamalbz <shaddam.albz@gmail.com>
2023-09-07 13:52:16 +0700

    feat: list user

--- src/pages/admin/UserManagement/UserManagement.tsx
+++ src/pages/admin/UserManagement/UserManagement.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { TableLowcode } from 'alurkerja-ui'
+import { useState } from 'react'
 
-const UserManagement = () => {
-  return <div>UserManagement</div>
+function UserManagement() {
+  const [pageConfig, setPageConfig] = useState({ limit: 10, page: 0 })
+  const [renderState, setRenderState] = useState(0)
+  const [filterBy, setFilterBy] = useState<{ [x: string]: any }>()
+  const [search, setSearch] = useState<string>()
+
+  return (
+    <section className="bg-white">
+      <TableLowcode
+        baseUrl={import.meta.env.VITE_API_BASEURL}
+        tableName="user"
+        renderState={renderState}
+        setRenderState={setRenderState}
+        pageConfig={pageConfig}
+        setPageConfig={setPageConfig}
+        filterBy={filterBy}
+        setFilterBy={setFilterBy}
+        search={search}
+        setSearch={setSearch}
+      />
+    </section>
+  )
 }
 
 export default UserManagement
