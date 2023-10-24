234c9076c5913ec720dad94ebcd5ec7541e16557
shaddamalbz <shaddam.albz@gmail.com>
2023-10-24 09:03:00 +0700

    fix: hide button delete & edit

--- src/pages/keuangan/AkunManagement.tsx
+++ src/pages/keuangan/AkunManagement.tsx
@@ -3,27 +3,29 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export const AkunManagement = () => {
-
   const [pageConfig, setPageConfig] = useState({ limit: 10, page: 0 })
   const [renderState, setRenderState] = useState(0)
   const [filterBy, setFilterBy] = useState<{ [x: string]: any }>()
   const [search, setSearch] = useState<string>()
-  return <>
-  
-  <section className="bg-white">
-      <TableLowcode
-        baseUrl={import.meta.env.VITE_API_BASEURL}
-        tableName="akun"
-        module="akun"
-        renderState={renderState}
-        setRenderState={setRenderState}
-        pageConfig={pageConfig}
-        setPageConfig={setPageConfig}
-        filterBy={filterBy}
-        setFilterBy={setFilterBy}
-        search={search}
-        setSearch={setSearch}
-      />
-    </section>
-  </>
+  return (
+    <>
+      <section className="bg-white">
+        <TableLowcode
+          baseUrl={import.meta.env.VITE_API_BASEURL}
+          tableName="akun"
+          module="akun"
+          renderState={renderState}
+          setRenderState={setRenderState}
+          pageConfig={pageConfig}
+          setPageConfig={setPageConfig}
+          filterBy={filterBy}
+          setFilterBy={setFilterBy}
+          search={search}
+          setSearch={setSearch}
+          customButtonDelete={() => <></>}
+          customButtonEdit={() => <></>}
+        />
+      </section>
+    </>
+  )
 }
